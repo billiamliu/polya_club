c36242e7c4311c089e2653a13766a9253445e2ca
billiam <hello@billiam.io>
2017-10-13 12:18:08 -0700

    can parse 1

--- 93.scm
+++ 93.scm
@@ -1,35 +1,27 @@
 (load "util.scm")
 (define input-str "25525511135")
 (define input (string->int-list input-str))
+input
 
 ;; input int list e.g. '(1 2 3)
 (define (is-valid-block? ints)
   (let ((block (int-list->int ints)))
     (and (>= block 0) (<= block 255))))
 
-; ok
-(define (take-up-to n current ints)
-  (if (or (null? ints) (= n 0))
-    current
-    (take-up-to (- n 1) (append current (list (car ints))) (cdr ints))))
-
-(define (drop-up-to n remain)
-  (if (or (null? remain) (= n 0))
-    remain
-    (drop-up-to (- n 1) (cdr remain))))
-
-(define block-size 3)
-
 (define (try input)
   (define (iter ints size)
+          ; null check
     (cond ((null? ints) '())
+          ; null check
           ((= 0 size) '())
+          ; start with 0 check
           ((= 0 (car ints))
            (cons '(0) (iter (cdr ints) size)))
-          ((is-valid-block? (take-up-to size '() ints))
+          ; main check
+          ((is-valid-block? (take-up-to size ints))
            (cons
-             (take-up-to size '() ints)
-             (iter (drop-up-to size ints) size)))
+             (take-up-to size ints)
+             (iter (drop-up-to size ints) 3)))
           (else (iter ints (- size 1)))))
   (iter input 3))
 
@@ -38,9 +30,12 @@
 (define input-2 '(0 0 0 0))
 (define input-3 '(2 5 5 2 5 5 2 5 5 2 5 5))
 (define input-4 '(2 5 5 2 5 5 1 1 1 3 5))
+(define input-5 '(2 5 6 5 0 1 1 1))
 
-(try input-0)
-(try input-1)
-(try input-2)
-(try input-3)
-(try input-4)
+;(try input)
+;(try input-0)
+;(try input-1)
+;(try input-2)
+;(try input-3)
+;(try input-4)
+(try input-5)
--- util.scm
+++ util.scm
@@ -1,6 +1,8 @@
+; ok
 (define (string->int-list str)
   (map (lambda (char) (- (char->integer char) 48)) (string->list str)))
 
+; ok
 (define (int-list->int ll)
   (let ((str-list (map number->string ll)))
     (define (iter strs)
@@ -8,3 +10,17 @@
         ""
         (string-append (car strs) (iter (cdr strs)))))
     (string->number (iter str-list))))
+
+; ok
+(define (take-up-to n ints)
+  (define (iter n ints current)
+    (if (or (null? ints) (= n 0))
+      current
+      (iter (- n 1) (cdr ints) (append current (list (car ints))))))
+  (iter n ints '()))
+
+; ok
+(define (drop-up-to n remain)
+  (if (or (null? remain) (= n 0))
+    remain
+    (drop-up-to (- n 1) (cdr remain))))
